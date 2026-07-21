8c5d33eb5dcee01e9aca5ef6747df23c0287cdba
LitenPanJun <xiaopanjun233@163.com>
2026-07-22 00:55:53 +0800

    feat: 自动拉取 OpenClash 和 EasyTier 全部在线编译

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -1,5 +1,5 @@
 function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev | sed 's/\.git$//'`
     pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
     find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
     git clone --depth=1 --single-branch $1
@@ -9,12 +9,15 @@ function merge_package(){
 
 rm -rf package/custom; mkdir package/custom
 
-# luci-theme-glass - from community source
-# https://github.com/rchen14b/luci-theme-glass
+# luci-theme-glass - Apple 风格玻璃主题
 merge_package https://github.com/rchen14b/luci-theme-glass luci-theme-glass
 
-# luci-app-openclash 和 luci-app-easytier 请手动拷贝至 package/ 目录下
-# 拷贝命令示例:
-#   cp -r /path/to/luci-app-openclash openwrt/package/
-#   cp -r /path/to/luci-app-easytier openwrt/package/
-#   cp -r /path/to/easytier openwrt/package/
+# luci-app-openclash - OpenClash 控制面板
+merge_package https://github.com/vernesong/OpenClash.git OpenClash/luci-app-openclash
+
+# EasyTier 全家桶 (luci-app-easytier + easytier 后端)
+# 两个包来自同一仓库的不同子目录，一次性克隆后分别移动
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git /tmp/easytier-full
+mv /tmp/easytier-full/luci-app-easytier package/custom/
+mv /tmp/easytier-full/easytier package/custom/
+rm -rf /tmp/easytier-full
